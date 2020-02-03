4a52b11f76ff1b89b369e33bf8d773571ab05d32
Candace <2100978@talnet.nl>
2020-02-03 01:17:50 +0100

    Uitwerking van de exercise

---
+++ calculator.php
@@ -0,0 +1,97 @@
+<?php
+
+
+$Operation = readline("Welke operatie wil je uitvoeren? (+, -, %)");
+
+
+if ($Operation !== "-") {
+    if ($Operation !== "+") {
+        if ($Operation !== "%") {
+
+
+            echo ($Operation . " Is not an operator");
+            exit;
+        }
+    }
+}
+
+
+if ($Operation === "+") {
+
+
+
+    $getal1 = readline("Eerste getal?");
+
+
+
+    if (is_numeric($getal1)) {
+    } else {
+        echo ($getal1 . "is geen getal");
+        exit;
+    }
+
+    $getal2 = readline("Tweede getal?");
+
+
+    if (is_numeric($getal2)) {
+    } else {
+        echo ($getal2 . "is geen getal");
+        exit;
+    }
+
+    $output = $getal1 + $getal2;
+}
+
+if ($Operation === "-") {
+
+    $getal1 = readline("Eerste getal?");
+
+
+    if (is_numeric($getal1)) {
+    } else {
+        echo ($getal1 . "is geen getal");
+        exit;
+    }
+
+    $getal2 = readline("Tweede getal?");
+
+    if (is_numeric($getal2)) {
+    } else {
+        echo ($getal2 . "is geen getal");
+        exit;
+    }
+
+
+    $output = $getal1 - $getal2;
+}
+
+if ($Operation === "%") {
+
+    $getal1 = readline("Eerste getal?");
+
+    if (is_numeric($getal1)) {
+    } else {
+        echo ($getal1 . "is geen getal");
+        exit;
+    }
+
+    $getal2 = readline("Tweede getal?");
+
+    if (is_numeric($getal2)) {
+    } else {
+        echo ($getal2 . "is geen getal");
+        exit;
+    }
+
+
+    $output = $getal1 % $getal2;
+}
+
+
+
+
+
+
+
+
+echo ("Het antwoord is:" . $output);
